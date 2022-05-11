6b9f4961425e93ee6bfeda5bbb41f6e7c7ce40f0
parkhohyun <haor3@hanmail.net>
2022-05-11 20:47:19 +0900

    뮤직플레이어 완성 / gitignore 추가

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     var timer: Timer!
     
     // MARK: IBOutlets
-    @IBOutlet var playPauseBotton: UIButton!
+    @IBOutlet var playPasueButton: UIButton!
     @IBOutlet var timeLabel: UILabel!
     @IBOutlet var progressSlider: UISlider!
     
@@ -45,7 +45,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         let second: Int = Int(time.truncatingRemainder(dividingBy: 60))
         let milisecond: Int = Int(time.truncatingRemainder(dividingBy: 1) * 100)
         
-        let timeText: String = String(format: "%021d:%021d:%021d", minute, second, milisecond)
+        let timeText: String = String(format: "%02ld:%02ld:%02ld", minute, second, milisecond)
         
         self.timeLabel.text = timeText
     }
@@ -77,24 +77,56 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     @IBAction func tocuhPlayPasueButton(_ sender: Any) {
         print("button tapped")
     }
-    @IBAction func sliderValueChanged(_ sender: UISlider) {
-        print("slider value changed")
-    }
-    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
-        
-        sender.isSelected = !sender.isSelected
-        
-        if sender.isSelected {
-            self.player?.play()
-        } else {
-            self.player?.pause()
+        @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+            
+            sender.isSelected = !sender.isSelected
+            
+            if sender.isSelected {
+                self.player?.play()
+            } else {
+                self.player?.pause()
+            }
+            
+            if sender.isSelected {
+                self.makeAndFireTimer()
+            } else {
+                self.invalidateTimer()
+            }
         }
-        
-        if sender.isSelected {
-            self.makeAndFireTimer()
-        } else {
-            self.invalidateTimer()
-        }
-    }
     
-}
+        @IBAction func sliderValueChanged(_ sender: UISlider) {
+            self.updateTimeLabelText(time: TimeInterval(sender.value))
+            if sender.isTracking { return }
+            self.player.currentTime = TimeInterval(sender.value)
+        }
+        
+        // MARK: AVAudioPlayerDelgegate
+        func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+            
+            guard let error: Error = error else {
+                print("오디오 플레이어 디코드 오류발생")
+                return
+            }
+            
+            let message: String
+            message = "오디오 플레이어 오류 발생 \(error.localizedDescription)"
+            
+            let alert: UIAlertController = UIAlertController(title: "알림", message: message, preferredStyle: UIAlertController.Style.alert)
+            
+            let okAction : UIAlertAction = UIAlertAction(title: "알림", style: UIAlertAction.Style.default) { (action: UIAlertAction) -> Void in
+                
+                self.dismiss(animated: true, completion: nil)
+                
+            }
+            
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+            self.playPasueButton.isSelected = false
+            self.progressSlider.value = 0
+            self.updateTimeLabelText(time: 0)
+            self.invalidateTimer()
+        }   
+    }
